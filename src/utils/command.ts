3511dd02728ff7bc0264a7225b9215776b6b3998
chanloveit <ch20041001@gmail.com>
2026-02-11 01:08:23 +0000

    chore: fix command guide

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -1,8 +1,7 @@
 export function commands(): void{
 	console.log('Commands: ');
-	console.log('/servers - List all servers');
-	console.log('/channels <Server ID> - List text channels in a server');
-	console.log('/join <Channel ID> - Join a channel to chat');
+	console.log('/servers - List all servers and channels');
+	console.log('/join <server>:<channel> - Join a channel to chat');
 	console.log('/leave - Leave current channel');
 	console.log('/help - Show commands');
 	console.log('/quit - Exit\n');
